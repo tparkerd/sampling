7ae0aa748c32f31201fce433cc6e7a1492391a04
tim <tim.parkerd@gmail.com>
2017-12-11 20:52:45 -0500

    user list and history drafts

---
+++ routes/user/list.js
@@ -0,0 +1,32 @@
+'use strict'
+const router     = require('express').Router(),
+      path       = require('path'),
+      mysql      = require('mysql'),
+      dbconfig   = require('../../config/database.js')
+
+router.get('/', (req, res) => {
+  // Connect and set database
+  let connection = mysql.createConnection(dbconfig.connection)
+  connection.query('USE ' + dbconfig.database)
+
+  let query = 'SELECT * FROM users;'
+  connection.query(query, (err, rows) => {
+    if (err) {
+      return req.flash('error', err)
+    }
+
+    console.log(rows);
+
+    // Got data back
+    if (rows.length) {
+      return res.render('list', { data: rows })
+    }
+  })
+
+
+
+
+
+})
+
+module.exports = router
