c39a181a6266c3d8785de51f49c25a84a3f2fcee
tim <tim.parkerd@gmail.com>
2018-01-07 09:15:08 -0500

    Removed post route for githook

--- routes/index.js
+++ routes/index.js
@@ -6,10 +6,6 @@ const router = require('express').Router(),
 // User account
 router.get('/', (req, res) => { res.render('index') })
 
-router.post('/postreceive', (req, res) => {
-	return res.status(200).json({message: 'received message'})
-})
-
 // Sub-routes
 router.use('/user', require('./user'))
 router.use('/observation', helper.isAuthenticated, require('./observation'))
