6eca98d32a09915ef980450efb061d11093c746f
tim <tim.parkerd@gmail.com>
2018-01-12 11:05:28 -0500

    Revert "Try/catch for database creation script"
    This reverts commit dbc5f6ad7df767b1d793ff21cbdc92024df50c5d.

--- scripts/create_database.js
+++ scripts/create_database.js
@@ -2,48 +2,38 @@ const mysql    = require('mysql'),
       dbconfig = require('./config/database.js')
 
 let connection = mysql.createConnection(dbconfig.connection)
+connection.query('DROP DATABASE IF EXISTS ' + dbconfig.database)
+connection.query('CREATE DATABASE ' + dbconfig.database)
 
-try {
-  let query = 'CREATE DATABASE ' + dbconfig.database
-  connection.query(query, (err, rows) => {
-    if (err) throw err
-  })
+// User table
+connection.query(`
+  CREATE TABLE  ${dbconfig.database}.${dbconfig.user_table} (
+    id INT UNSIGNED NOT NULL AUTO_INCREMENT,
+    alias VARCHAR(120),
+    email VARCHAR(120) NOT NULL,
+    password CHAR(60) NOT NULL,
+    PRIMARY KEY (id),
+    UNIQUE INDEX id_UNIQUE (id ASC),
+    UNIQUE INDEX email_UNIQUE (email ASC)
+    )
+  `)
 
-  // User table
-  query = `
-    CREATE TABLE  ${dbconfig.database}.${dbconfig.user_table} (
-      id INT UNSIGNED NOT NULL AUTO_INCREMENT,
-      alias VARCHAR(120),
-      email VARCHAR(120) NOT NULL,
-      password CHAR(60) NOT NULL,
-      PRIMARY KEY (id),
-      UNIQUE INDEX id_UNIQUE (id ASC),
-      UNIQUE INDEX email_UNIQUE (email ASC)
-      )
-    `
-  connection.query(query, (err, rows) => {
-    if (err) throw err
-  })
+// Samples / Observations
 
-  // Samples / Observations
-  query = `
-    CREATE TABLE ${dbconfig.database}.${dbconfig.classification_table} (
-      id INT UNSIGNED NOT NULL AUTO_INCREMENT,
-      sample_id INT NOT NULL,
-      user_id INT NOT NULL,
-      rating INT NOT NULL,
-      notes TEXT,
-      type_of_post VARCHAR(25) NOT NULL,
-      PRIMARY KEY (id),
-      UNIQUE INDEX id_UNIQUE2 (id ASC)
-      )
-    `
-  connection.query(query, (err, rows) => {
-    if (err) throw err
-  })
-  console.log('Success: Database created!')
-} catch (e) {
-  console.log('Failed: Database was NOT created.')
-} finally {
-  connection.end()
-}
+// Evaluations / Classifications
+connection.query(`
+  CREATE TABLE ${dbconfig.database}.${dbconfig.classification_table} (
+    id INT UNSIGNED NOT NULL AUTO_INCREMENT,
+    sample_id INT NOT NULL,
+    user_id INT NOT NULL,
+    rating INT NOT NULL,
+    notes TEXT,
+    type_of_post VARCHAR(25) NOT NULL,
+    PRIMARY KEY (id),
+    UNIQUE INDEX id_UNIQUE2 (id ASC)
+    )
+  `)
+
+console.log('Success: Database created!')
+
+connection.end()
