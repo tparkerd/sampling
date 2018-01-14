4870cdd84fe21f8befa7b67e24c652b9ac8a1eb8
tim <tim.parkerd@gmail.com>
2018-01-14 02:43:42 -0500

    implemented post details

--- routes/observation/details.js
+++ routes/observation/details.js
@@ -4,92 +4,42 @@ const router = require('express').Router(),
       dbconfig   = require('../../config/database.js')
 
 
-      router.get('/:id', (req, res) => {
-        let connection = mysql.createConnection(dbconfig.connection)
-        connection.query('USE ' + dbconfig.database)
-          let query = `SELECT c.*, p.*
-                       FROM classifications c
-                       INNER JOIN reddit.posts p
-                        ON p._id = c.sample_id
-                       WHERE c.id = ?
-                       `
-        let data = {}
 
-        // NOTE: consider escaping the req.param.id
-        connection.query(query, [req.params.id], (err, rows) => {
-          if (err) return req.flash('error', 'Unknown error occurred while accessing database.')
+router.get('/:id', (req, res) => {
+  dbconfig.connection.multipleStatements = true
+  let connection = mysql.createConnection(dbconfig.connection)
+  let query
+  query = `SELECT *
+           FROM classifier.classifications c
+           INNER JOIN reddit.posts p
+            ON p._id = c.sample_id
+           WHERE c.id = ?;
 
-          if (rows.length) {
-            data.info = rows
-          }
+           SELECT c.id, c.rating, c.notes, u.alias AS rater, u.id as user_id
+           FROM classifier.classifications c
+           INNER JOIN classifier.users u
+            ON u.id = c.user_id
+           WHERE c.id = ?;
 
-        connection.query('USE classifier')
-        query = `SELECT c.id, c.rating, c.notes, u.alias AS rater, u.id as user_id
-                 FROM classifier.classifications c
-                 INNER JOIN classifier.users u
-                  ON u.id = c.user_id
-                 WHERE c.id = ?
-                `
-          connection.query(query, [req.params.id], (err, rows) => {
-            if (err) return req.flash('error', err)
+           SELECT AVG(c.rating) AS mean
+           FROM classifier.classifications c
+           WHERE c.id = ?;
+           `
+  connection.query(query, [req.params.id, req.params.id, req.params.id], (err, rows) => {
+    if (err) {
+      req.flash('error', 'Unknown error occurred while accessing database.')
+      return res.render('observation/details', { data: { info: {}, evaluations: {} } })
+    }
 
-            // Got data back
-            if (rows.length) {
-              console.log(rows);
-              data.evaluations = rows
-            }
-            return res.render('observation/details', { data: data })
-          })
-        })
-      })
+    if (rows.length > 0) {
+      results = {}
+      results.info = rows[0][0]
+      results.info.averageRating = rows[2][0].mean
+      results.evaluations = rows[1]
+      return res.render('observation/details', { data: results })
+    }
+  })
+})
 
-
-// router.get('/:id', (req, res) => {
-//   let results = {}
-//
-//   let connection = mysql.createConnection(dbconfig.connection)
-//   connection.query('USE classifier')
-//
-//   let query = `SELECT c.*, p.*
-//                FROM classifications c
-//                INNER JOIN reddit.posts p
-//                 ON p._id = c.sample_id
-//                WHERE c.id = ?
-//                `
-//   results.info = []
-//   connection.query(query, [ req.params.id ] ,(err, rows) => {
-//     if (err) {
-//       return req.flash('error', err)
-//     }
-//
-//     // Got data back
-//     if (rows.length) {
-//       for (let r in rows) {
-//         results.info[r] = rows[r]
-//       }
-//     }
-//   })
-//
-//   // Get history of evaluations of the observation
-//   query = `SELECT c.id, c.rating, c.notes, u.alias AS rater, u.id as user_id
-//            FROM classifier.classifications c
-//            INNER JOIN classifier.users u
-//             ON u.id = c.user_id
-//            WHERE c.id = ?
-//           `
-//
-//   results.evaluations = []
-//   connection.query(query, [ req.params.id ], (err, rows) => {
-//     if (err) return req.flash('error', err)
-//
-//     if (rows.length) {
-//       results.evaluations = rows
-//     }
-//   })
-//
-//   console.log(results);
-//   return res.render('observation/details', { data: results })
-//   // res.render('404')
-// })
 
 module.exports = router
