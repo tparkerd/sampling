cfcfdee100e35f225514e4c966d20881ab860c12
Tim P <Tim.ParkerD@gmail.com>
2018-01-14 23:33:43 -0500

    Clean up

--- routes/observation/index.js
+++ routes/observation/index.js
@@ -1,9 +1,6 @@
 const router = require('express').Router(),
       path   = require('path')
 
-
-// TODO: Actually pull a random observation from the samples
-
 router.use('/details', require('./details.js'))
 router.use('/edit', require('./edit.js'))
 router.use('/evaluate', require('./evaluate.js'))
