c66745969bbee4a7e149340e99e9070f420916d5
Tim P <Tim.ParkerD@gmail.com>
2018-01-31 01:08:56 -0500

    Added sample listing page

--- routes/observation/index.js
+++ routes/observation/index.js
@@ -4,5 +4,6 @@ const router = require('express').Router(),
 router.use('/details', require('./details.js'))
 router.use('/evaluate', require('./evaluate.js'))
 router.use('/history', require('./history.js'))
+router.use('/list', require('./list.js'))
 
 module.exports = router
--- routes/observation/list.js
+++ routes/observation/list.js
@@ -3,14 +3,70 @@ const router = require('express').Router(),
       mysql      = require('mysql'),
       dbconfig   = require('../../config/database.js')
 
-
-// TODO: Show all observations
-// NOTE: this may not be necessary if I don't first pull a sample batch
-//       from the reddit.posts and comments tables. If I choose to pull
-//       a random one each time, then this won't be necessary to exist
-
 router.get('/', (req, res) => {
-  res.render('observation/list')
+  // Connect and set database
+  let connection = mysql.createConnection(dbconfig.connection)
+
+  let query = `SELECT s._id AS postId,
+                      s.content_text AS contents,
+                      AVG(c.rating) AS avgRating
+               FROM reddit.samples s
+               LEFT JOIN classifier.classifications c
+                ON s._id = c.sample_id
+               LEFT JOIN classifier.users u
+                ON c.user_id = u.id
+               GROUP BY s._id;
+               `
+  let data = {}
+  connection.query(query, (err, rows) => {
+    if (err) {
+      req.flash('error', err)
+      return res.render('index')
+    }
+
+    data.observations = []
+          // Got data back
+          if (rows.length) {
+            // Provide a shortened version for the view
+            for (let i in rows) {
+              if (rows[i].contents.length > 85) {
+                // Get the first space after the first 85 characters
+                let cutoffIndex = rows[i].contents.indexOf(' ', 85)
+                if (cutoffIndex > 120) cutoffIndex = 65
+                rows[i].shorttext = rows[i].contents.substring(0, cutoffIndex) + '...'
+              }
+              else
+                rows[i].shorttext = rows[i].contents
+            }
+            data.observations = rows
+          }
+          return res.render('observation/list', { data: data.observations })
+  })
+})
+
+
+router.get('/export/', (req, res) => {
+  // Connect and set database
+  let connection = mysql.createConnection(dbconfig.connection)
+  let query = `SELECT s._id AS postId,
+                      s.content_text AS contents,
+                      AVG(c.rating) AS avgRating
+               FROM reddit.samples s
+               LEFT JOIN classifier.classifications c
+                ON s._id = c.sample_id
+               LEFT JOIN classifier.users u
+                ON c.user_id = u.id
+               GROUP BY s._id
+               `
+    connection.query(query, [req.params.id], (err, rows) => {
+      if (err) return req.flash('error', err)
+
+      // Got data back
+      if (rows.length) {
+        let headers = { "Content-Disposition": 'attachment;filename=sample_data.csv' }
+        res.csv(rows, true, headers)
+      }
+    })
 })
 
 module.exports = router
