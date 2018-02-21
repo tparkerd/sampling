cd10eb2e6809975d3cc17c36c7f036f1c54846e7
Tim Parker <tparkerd@gmail.com>
2018-02-21 03:49:50 -0500

    Real data now sent to statistic page for individual counts of annotations

--- routes/observation/statistics.js
+++ routes/observation/statistics.js
@@ -34,7 +34,20 @@ router.get('/', (req, res) => {
       sampleCounts: []
     }
     // Got data back
+    let maps = new Array(4)
+    for (let i = 0; i < maps.length; i++) maps[i] = new Map()
     if (rows.length) {
+      // Add every user to the maps
+      for (let i = 0; i < rows.length; i++) {
+        // For each level of severity
+        for (let j = 0; j < maps.length; j++) {
+          // If user hasn't already been added, add them
+          if (!maps[j].has(rows[i].alias)) {
+            maps[j].set(rows[i].alias, 0)
+          }
+        }
+      }
+
       for (let i in rows) {
         data.users.names.add(rows[i].alias)
         switch (rows[i].rating) {
@@ -53,7 +66,13 @@ router.get('/', (req, res) => {
           default:
             break
         }
+        // Get the last value that the person had for this rating
+        //             -- severity --      --   user  --
+        let tmp = maps[rows[i].rating].get(rows[i].alias) || 0
+        //   -- severity --      --   user  --
+        maps[rows[i].rating].set(rows[i].alias, tmp + 1)
       }
+
       for (let i in rows) {
         // Collect the classified samples under each user
         if (data.users.samples[rows[i].user_id] === undefined)
@@ -71,6 +90,12 @@ router.get('/', (req, res) => {
     for (let i in data.users.samples) {
       data.users.counts[i - 1] = data.users.samples[i].length
     }
+
+    // Convert maps to dataset format
+    data.samples.absent = Array.from(maps[0].values())
+    data.samples.mild = Array.from(maps[1].values())
+    data.samples.moderate = Array.from(maps[2].values())
+    data.samples.severe = Array.from(maps[3].values())
 
     // Convert names of users to an array of strings to be used as labels
     let tmp = Array.from(data.users.names) // convert set of users to an array
